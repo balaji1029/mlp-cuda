3afd23398157a660f738bea1b8cc88d31cb1cd99
Balaji <balajikaredla4@gmail.com>
2026-03-16 15:50:57 +0530

    try push

---
+++ mlp_streamed.cu
@@ -0,0 +1,151 @@
+#include <cuda_runtime.h>
+#include <string>
+#include <iostream>
+
+#define NELEM 6
+#define BLOCK_SIZE 32
+#define CEIL_DIV(a, b) (a + b - 1) / b
+
+#define CHECK_CUDA(x) \
+{ \
+    cudaError_t err = x; \
+    if(err != cudaSuccess){ \
+        std::cerr << cudaGetErrorString(err) << std::endl; \
+        exit(1); \
+    } \
+}
+
+__global__ void relu_kernel(float* data, size_t M, size_t N) {
+    int globalX = blockDim.x * blockIdx.x + threadIdx.x;
+    int globalY = blockDim.y * blockIdx.y + threadIdx.y;
+
+    if (globalX < N && globalY < M) {
+        size_t idx = globalY * N + globalX;
+        data[idx] = fmaxf(0.0f, data[idx]);
+    }
+}
+
+__global__ void tiling_matmul(const float* A, const float* B, float* C, size_t M, size_t N, size_t K) {
+    __shared__ float tileA[NELEM * BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float tileB[BLOCK_SIZE][BLOCK_SIZE];
+
+    int localX = threadIdx.x;
+    int localY = threadIdx.y;
+
+    int globalX = blockDim.x * blockIdx.x + localX;
+    int globalY = blockDim.y * (blockIdx.y * NELEM) + localY;
+
+    float ans[NELEM] = { 0.0f };
+
+    int numTiles = CEIL_DIV(K, BLOCK_SIZE);
+
+    for (int i = 0; i < numTiles; i++) {
+        int tileAx = i * BLOCK_SIZE + localX;
+        int tileAy = globalY;
+
+        for (int k = 0; k < NELEM; k++) {
+            tileA[localY + k * BLOCK_SIZE][localX] = ((tileAy + k * BLOCK_SIZE) < M && tileAx < K) ? A[(tileAy + k * BLOCK_SIZE) * K + tileAx] : 0.0f;
+        }
+
+        int tileBx = globalX;
+        int tileBy = i * BLOCK_SIZE + localY;
+
+        tileB[localY][localX] = (tileBy < K && tileBx < N) ? B[tileBy * N + tileBx] : 0.0f;
+
+        __syncthreads();
+
+        for (int j = 0; j < BLOCK_SIZE; j++) {
+            for (int k = 0; k < NELEM; k++) {
+                ans[k] += tileA[localY + k * BLOCK_SIZE][j] * tileB[j][localX];
+            }
+        }
+
+        __syncthreads();
+    }
+
+    for (int k = 0; k < NELEM; k++)
+        if ((globalY + k * BLOCK_SIZE) < M && globalX < N)
+            C[(globalY + k * BLOCK_SIZE) * N + globalX] = ans[k];
+}
+
+int main(int argc, char** argv) {
+    size_t N = 1024, B = 32;
+    if (argc == 2) {
+        N = std::stoul(argv[1]);
+    }
+
+    float* W1 = new float[N * N * sizeof(float)];
+    float* W2 = new float[(N * N * sizeof(float))];
+
+    float* input[4];
+    for (int i = 0; i < 4; i++) {
+        input[i] = new float[(N * (B / 4) * sizeof(float))];
+    }
+
+    for (int i = 0; i < N * N; i++) {
+        W1[i] = static_cast<float>(rand()) / RAND_MAX;
+        W2[i] = static_cast<float>(rand()) / RAND_MAX;
+    }
+
+    for (int i = 0; i < N * (B / 4); i++) {
+        for (int j = 0; j < 4; j++) {
+            input[j][i] = static_cast<float>(rand()) / RAND_MAX;
+        }
+    }
+
+    float* d_W1, * d_W2;
+
+    float* d_input[4];
+    float* d_output1[4];
+    float* d_output2[4];
+
+    cudaMalloc(&d_W1, N * N * sizeof(float));
+    cudaMalloc(&d_W2, N * N * sizeof(float));
+
+    cudaMemcpy(d_input, input, N * B * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_W1, W1, N * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_W2, W2, N * N * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 gridSize1(CEIL_DIV(B, BLOCK_SIZE), CEIL_DIV(N, NELEM * BLOCK_SIZE));
+    dim3 blockSize1(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 gridSize_relu(CEIL_DIV(B, BLOCK_SIZE), CEIL_DIV(N, BLOCK_SIZE));
+    dim3 blockSize_relu(BLOCK_SIZE, BLOCK_SIZE);
+
+    cudaStream_t stream[4];
+    cudaEvent_t start, end;
+    for (int i = 0; i < 4; i++) {
+        cudaStreamCreate(&stream[i]);
+    }
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+    for (int i=0; i<4; i++) {
+        cudaEventRecord(start, stream[i]);
+        cudaMemcpyAsync(d_input[i], input[i], N * (B / 4) * sizeof(float), cudaMemcpyHostToDevice, stream[i]);
+        tiling_matmul << < gridSize1, blockSize1, 0, stream[i] >> > (d_W1, d_input[i], d_output1[i], N, (B / 4), N);
+        relu_kernel << < gridSize_relu, blockSize_relu, 0, stream[i] >> > (d_output1[i], N, (B / 4));
+        tiling_matmul << < gridSize1, blockSize1, 0, stream[i] >> > (d_W2, d_output1[i], d_output2[i], N, (B / 4), N);
+        cudaEventRecord(end, stream[i]);
+    }
+    for (int i=0; i<4; i++) {
+        cudaStreamSynchronize(stream[i]);
+    }
+    float elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, start, end);
+
+    std::cout << elapsedTime << std::endl;
+
+    delete[] W1;
+    delete[] W2;
+    delete[] input;
+
+    cudaFree(d_W1);
+    cudaFree(d_W2);
+    cudaFree(d_input);
+    cudaFree(d_output1);
+    cudaFree(d_output2);
+
+    cudaEventDestroy(start);
+
+    return 0;
+
+}
--- run.py
+++ run.py
@@ -3,20 +3,56 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     # Compile the CUDA code
-    subprocess.run(["nvcc", "-o", "mlp", "mlp.cu"], check=True)
+    # subprocess.run(["nvcc", "-o", "mlp", "mlp.cu"], check=True)
 
     batched = []
     N_sizes = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
     
     # Run with command line arguments from 32 to 32k in powers of 2
-    for N in N_sizes:
-        print(f"Running with N={N}")
-        subprocess.run(["./mlp", str(N)], check=True)
-        output = subprocess.check_output(["./mlp", str(N)])
-        # print(output.decode())
-        elapsedTime = float(output.decode().strip())
-        print(f"Elapsed time for N={N}: {elapsedTime} ms")
-        batched.append(elapsedTime)
+    # for N in N_sizes:
+    #     print(f"Running with N={N}")
+    #     subprocess.run(["./mlp", str(N)], check=True)
+    #     output = subprocess.check_output(["./mlp", str(N)])
+    #     # print(output.decode())
+    #     elapsedTime = float(output.decode().strip())
+    #     print(f"Elapsed time for N={N}: {elapsedTime} ms")
+    #     batched.append(elapsedTime)
+
+    # Running with N=32
+    # 5.01146
+    # Elapsed time for N=32: 2.98202 ms
+    # Running with N=64
+    # 3.1703
+    # Elapsed time for N=64: 3.38221 ms
+    # Running with N=128
+    # 3.92909
+    # Elapsed time for N=128: 8.28614 ms
+    # Running with N=256
+    # 6.47475
+    # Elapsed time for N=256: 3.86765 ms
+    # Running with N=512
+    # 5.42413
+    # Elapsed time for N=512: 5.5296 ms
+    # Running with N=1024
+    # 11.3531
+    # Elapsed time for N=1024: 13.9993 ms
+    # Running with N=2048
+    # 33.7715
+    # Elapsed time for N=2048: 33.3957 ms
+    # Running with N=4096
+    # 121.431
+    # Elapsed time for N=4096: 115.688 ms
+    # Running with N=8192
+    # 976.844
+    # Elapsed time for N=8192: 993.196 ms
+    # Running with N=16384
+    # 3.34336
+    # Elapsed time for N=16384: 3.21024 ms
+    # Running with N=32768
+    # 4.79453
+    # Elapsed time for N=32768: 4.4137 ms
+
+    batched = [2.98202, 3.38221, 8.28614, 3.86765, 5.5296, 13.9993, 33.3957, 115.688, 993.196, 3210.24, 4413.7]
     
     # Plot the results
     plt.figure(figsize=(10, 6))
