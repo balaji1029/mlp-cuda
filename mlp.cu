ace8935640f0668ce08be245d4128635717b9bf7
Balaji <balajikaredla4@gmail.com>
2026-03-16 15:18:34 +0530

    try again

--- mlp.cu
+++ mlp.cu
@@ -6,6 +6,15 @@
 #define BLOCK_SIZE 32
 #define CEIL_DIV(a, b) (a + b - 1) / b
 
+#define CHECK_CUDA(x) \
+{ \
+    cudaError_t err = x; \
+    if(err != cudaSuccess){ \
+        std::cerr << cudaGetErrorString(err) << std::endl; \
+        exit(1); \
+    } \
+}
+
 __global__ void relu_kernel(float* data, size_t M, size_t N) {
     int globalX = blockDim.x * blockIdx.x + threadIdx.x;
     int globalY = blockDim.y * blockIdx.y + threadIdx.y;
@@ -34,7 +43,7 @@ __global__ void tiling_matmul(const float* A, const float* B, float* C, size_t M
         int tileAx = i * BLOCK_SIZE + localX;
         int tileAy = globalY;
 
-        for (int k=0; k < NELEM; k++) {
+        for (int k = 0; k < NELEM; k++) {
             tileA[localY + k * BLOCK_SIZE][localX] = ((tileAy + k * BLOCK_SIZE) < M && tileAx < K) ? A[(tileAy + k * BLOCK_SIZE) * K + tileAx] : 0.0f;
         }
 
@@ -59,16 +68,16 @@ __global__ void tiling_matmul(const float* A, const float* B, float* C, size_t M
             C[(globalY + k * BLOCK_SIZE) * N + globalX] = ans[k];
 }
 
-int main(int argc, char**argv) {
+int main(int argc, char** argv) {
     size_t N = 1024, B = 32;
     if (argc == 2) {
         N = std::stoul(argv[1]);
     }
 
-    float* W1 = (float*)malloc(N * N * sizeof(float));
-    float* W2 = (float*)malloc(N * N * sizeof(float));
+    float* W1 = new float[N * N * sizeof(float)];
+    float* W2 = new float[(N * N * sizeof(float))];
 
-    float* input = (float*)malloc(N * B * sizeof(float));
+    float* input = new float[(N * B * sizeof(float))];
 
     for (int i = 0; i < N * N; i++) {
         W1[i] = static_cast<float>(rand()) / RAND_MAX;
@@ -95,7 +104,7 @@ int main(int argc, char**argv) {
     dim3 blockSize1(BLOCK_SIZE, BLOCK_SIZE);
     dim3 gridSize_relu(CEIL_DIV(B, BLOCK_SIZE), CEIL_DIV(N, BLOCK_SIZE));
     dim3 blockSize_relu(BLOCK_SIZE, BLOCK_SIZE);
-    
+
     cudaStream_t stream;
     cudaEvent_t start, end;
     cudaStreamCreate(&stream);
@@ -103,27 +112,36 @@ int main(int argc, char**argv) {
     cudaEventCreate(&end);
     cudaEventRecord(start, stream);
 
-    tiling_matmul <<< gridSize1, blockSize1, 0, stream >>> (d_W1, d_input, d_output1, N, B, N);
-    relu_kernel <<< gridSize_relu, blockSize_relu, 0, stream >>> (d_output1, N, B);
-    tiling_matmul <<< gridSize1, blockSize1, 0, stream >>> (d_W2, d_output1, d_output2, N, B, N);
+    tiling_matmul << < gridSize1, blockSize1, 0, stream >> > (d_W1, d_input, d_output1, N, B, N);
+    relu_kernel << < gridSize_relu, blockSize_relu, 0, stream >> > (d_output1, N, B);
+    tiling_matmul << < gridSize1, blockSize1, 0, stream >> > (d_W2, d_output1, d_output2, N, B, N);
 
     cudaEventRecord(end, stream);
     cudaStreamSynchronize(stream);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+        std::cout << cudaGetErrorString(err) << std::endl;
     float elapsedTime;
     cudaEventElapsedTime(&elapsedTime, start, end);
-    
+
     std::cout << elapsedTime << std::endl;
 
     // cudaMalloc(&d_W2, N * N * sizeof(float));
     // cudaMalloc(&d_output2, N * B * sizeof(float));
 
-    delete [] W1;
-    delete [] W2;
-    delete [] input;
+    delete[] W1;
+    delete[] W2;
+    delete[] input;
 
     cudaFree(d_W1);
+    cudaFree(d_W2);
     cudaFree(d_input);
     cudaFree(d_output1);
+    cudaFree(d_output2);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(end);
+    cudaStreamDestroy(stream);
 
     return 0;
 
