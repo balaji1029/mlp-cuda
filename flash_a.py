66e1c212784257a3c545a30bf29d5be5fc81d82d
Balaji <balajikaredla4@gmail.com>
2026-04-12 16:39:28 +0530

    flash attention naive

--- flash_a.py
+++ flash_a.py
@@ -16,10 +16,32 @@ def regular_attention(Q, K, V):
     A = torch.softmax(QK_T, dim=-1)
     return torch.matmul(A, V)
 
+def flash_attention_naive(Q, K, V):
+    M = 32 * 1024
+    Bc = M // (4 * D)
+    Br = min(Bc, D)
+    Tc = (N + Bc - 1) // Bc
+    Tr = (N + Br - 1) // Br
 
+    O = torch.zeros_like(Q)
+    l = torch.zeros((B, N_H, N, 1))
+    m = torch.zeros((B, N_H, N, 1))
+
+    for j in range(Tc):
+        K_j = K[:, :, j*Bc:(j+1)*Bc, :]
+        V_j = V[:, :, j*Bc:(j+1)*Bc, :]
+        for i in range(Tr):
+            Q_i = Q[:, :, i*Br:(i+1)*Br, :]
+            S_ij = torch.matmul(Q_i, K_j.transpose(-2, -1))
+            m_ij = torch.max(S_ij, dim=-1, keepdim=True).values
+            l_ij = torch.sum(torch.exp(S_ij - m_ij), dim=-1, keepdim=True)
+            O[:, :, i*Br:(i+1)*Br, :] += torch.matmul(torch.exp(S_ij - m_ij) / l_ij, V_j)
+            m[:, :, i*Br:(i+1)*Br, :] = torch.max(m[:, :, i*Br:(i+1)*Br, :], m_ij)
+            l[:, :, i*Br:(i+1)*Br, :] = l[:, :, i*Br:(i+1)*Br, :] * torch.exp(m[:, :, i*Br:(i+1)*Br, :] - m_ij) + l_ij * torch.exp(m[:, :, i*Br:(i+1)*Br, :] - m_ij)
+    return O
 
 if __name__ == "__main__":
-    for N in [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]:
+    for N in [32, 64, 128, 256, 512, 1024, 2048, 4096]:
         # Create Q, K, V in proper multi-head shape: (B, num_heads, seq_len, head_dim)
         Q = torch.randn(B, N_H, N, D, device=device, dtype=torch.float16)
         K = torch.randn(B, N_H, N, D, device=device, dtype=torch.float16)
@@ -42,5 +64,20 @@ if __name__ == "__main__":
         torch.cuda.synchronize()
 
         print(f"Regular attention time: {start.elapsed_time(end):.3f} ms")
+
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+
+        start.record()
+        output_flash = flash_attention_naive(Q, K, V)
+        end.record()
+
+        torch.cuda.synchronize()
+
+        if not torch.allclose(output, output_flash, atol=1e-3):
+            print("Outputs do not match!")
+
+        print(f"Flash attention time: {start.elapsed_time(end):.3f} ms")
+
     # print(f"Output shape: {output.shape}")           # Should be [8, 16, 32, 64]
     # print(f"Max memory: {torch.cuda.max_memory_allocated() / 1024**2:.1f} MB")
